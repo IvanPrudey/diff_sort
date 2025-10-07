7ceff454272e0dbb3eb83aa2d799d4e613db4271
IvanPrudey <prudey.ia@yandex.ru>
2025-10-07 08:43:21 +0300

    add_select_choice

--- app.py
+++ app.py
@@ -3,8 +3,17 @@ if __name__ == '__main__':
     print('1 пузырьковая')
     print('2 выбором')
 
-choice = input('выберите вариант сортировки для визуализации(1-2):')
 sort_types = {
     '1': 'bubble',
     '2': 'selection'
 }
+
+def select_choice(sort_types):
+    choice = input('выберите вариант сортировки для визуализации(1-2):')
+    if choice in sort_types:
+        pass
+    else:
+        print("неверный выбор, ")
+        select_choice(sort_types)
+
+select_choice(sort_types)
